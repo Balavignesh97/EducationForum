20f09311c8a5b8c9aac721fc569df32c0724e30e
Balavignesh97 <balavigneshp97@gmail.com>
2024-11-20 21:46:20 +0530

    Add project files.

---
+++ EducationForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
